68aa4b4638a08b90d3d8d07956e03140ffd9509d
fhergomez <fhergomez37@gmail.com>
2017-03-06 23:41:26 -0800

    work on bringing data from api to the front-end

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   console.log('JQuery is ready');
 
+  var url = "http://en.wikipedia.com/w/api.php?action=opensearch&search=" + searchTerm + "&format=jsoncallback=?";
+  var searchTerm = $('#searchTerm').val();
+
+  $('#search').on('click',function(){
+    $.ajax({
+      type: 'GET',
+      url: url,
+      async: false,
+      dataType: "json",
+      success: function(data){
+
+      },
+      error: function(errorMessage){
+        alert('error');
+      }
+    });
+  });
+
 
 })
