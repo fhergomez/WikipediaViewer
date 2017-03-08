801a94e1bb50226868ba6c1f2f0eb5485e43cd09
fhergomez <fhergomez37@gmail.com>
2017-03-07 23:43:29 -0800

    output data on to a list

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
   console.log('JQuery is ready');
 
-  var url = "http://en.wikipedia.com/w/api.php?action=opensearch&search=" + searchTerm + "&format=jsoncallback=?";
-  var searchTerm = $('#searchTerm').val();
-
   $('#search').on('click',function(){
+
+    var searchTerm = $('#searchTerm').val();
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+
     $.ajax({
       type: 'GET',
       url: url,
       async: false,
       dataType: "json",
       success: function(data){
+        console.log(data);
+        for (var i = 0; i <= data[0].length - 1; i++) {
+          console.log(data[1][i]);
+          $('#output').html(data[1][i]);
+        }
+
 
       },
       error: function(errorMessage){
