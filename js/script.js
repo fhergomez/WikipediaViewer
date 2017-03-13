da40f8590588e195a7abf7ad9899521e9b3e6b7c
fhergomez <fhergomez37@gmail.com>
2017-03-12 20:11:29 -0700

    worked on css

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
 $(document).ready(function(){
   console.log('JQuery is ready');
 
-  $('#search').on('click',function(){
 
+
+  $('#searchBtn').click(function() {
     var searchTerm = $('#searchTerm').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-
     $.ajax({
       type: 'GET',
       url: url,
-      async: false,
       dataType: "json",
-      success: function(data){
+      success: function(data) {
         console.log(data);
         $('#output').html('');
         for (var i = 0; i < data[1].length - 1; i++) {
-          console.log(data[1][i]);
-          $('#output').prepend('<li><a href=' +  data[3][i] + '>' + data[1][i] + '</a><p>' + data[2][i] + '</p></li>');
+          // console.log(data[1][i]);
+          $('#output').prepend('<li><a class="data" href=' +  data[3][i] + '>' + data[1][i] + '</a><p>' + data[2][i] + '</p></li>');
         }
-
-
       },
       error: function(errorMessage){
         console.log(errorMessage);
       }
     });
   });
-
-
-})
+});
