8c0afde119957209ada693e81fbbd49ccf133163
fhergomez <fhergomez37@gmail.com>
2017-03-12 23:32:33 -0700

    work on minor problems regarding events and responsive css

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   console.log('JQuery is ready');
 
 
-
   $('#searchBtn').click(function() {
     var searchTerm = $('#searchTerm').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
@@ -13,14 +12,26 @@ $(document).ready(function(){
       success: function(data) {
         console.log(data);
         $('#output').html('');
-        for (var i = 0; i < data[1].length - 1; i++) {
-          // console.log(data[1][i]);
-          $('#output').prepend('<li><a class="data" href=' +  data[3][i] + '>' + data[1][i] + '</a><p>' + data[2][i] + '</p></li>');
+        for (var i = 0; i < data[1].length; i++) {
+          $('#output').prepend('<li><a class="data" href=' +  data[3][i] + ' target="_blank">' + data[1][i] + '</a><p>' + data[2][i] + '</p></li>');
         }
       },
       error: function(errorMessage){
         console.log(errorMessage);
       }
     });
+
+    $('#searchTerm').on('focus', function (e) {
+      e.preventDefault();
+      $('#searchTerm').val('');
+    });
+
   });
+
+  $("#searchTerm").keyup(function(e) {
+    if (e.which == 13) {
+      $('#searchBtn').click();
+    }
+  });
+
 });
